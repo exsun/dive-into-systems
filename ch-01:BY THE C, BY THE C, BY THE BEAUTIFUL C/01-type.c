d408d01ceb30d9c09d4070a5bd78713071ccbd6f
ehsangh7 <ehsanghochagh@gmail.com>
2023-04-07 23:52:24 +0330

    :zap: update types

--- ch-01:BY THE C, BY THE C, BY THE BEAUTIFUL C/01-type.c
+++ ch-01:BY THE C, BY THE C, BY THE BEAUTIFUL C/01-type.c
@@ -1,11 +1,25 @@
+/* 
+ Types in C
+*/
+
 #include <stdio.h>
 #include <math.h>
 
 int main(){
 	int x;
 	float y;
+	char s;
+	long l;
+	long long lo;
+	double d;
+
 	printf("size of int %ld \n", sizeof(x));
 	printf("size of float %ld \n", sizeof(y));
+	printf("size of char %ld \n", sizeof(s));
+	printf("size of long %ld \n", sizeof(l));
+	printf("size of long long %ld \n", sizeof(lo));
+	printf("size of double %ld \n", sizeof(d));
+
 
 	return 0;
 }
