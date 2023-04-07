9f503a40fa4ef8436d1aee79317fcb2ee75f2d16
ehsangh7 <ehsanghochagh@gmail.com>
2023-04-07 22:23:01 +0330

    hello world in C

---
+++ ch-01/01-hello.c
@@ -0,0 +1,14 @@
+/*
+The Hello World Program in C
+*/
+#include <math.h>
+#include <stdio.h>
+
+/* main function definition: */
+int main() {
+    // statistics end in a semicolon (;)
+    printf("Hello World\n");
+    printf("sqrt(4) is :%f\n", sqrt(4));
+
+    return 0; // main return 0
+}
